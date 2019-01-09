73a5879f2bfc92511d902e33e1ec3fa3eaa1d58e
Unknown <1cheaterok1@gmail.com>
2019-01-10 00:21:20 +0300

    Добавляет все страницы

--- web/js/react_components.js
+++ web/js/react_components.js
@@ -1,24 +1,188 @@
 'use strict';
 
-class MainScreen extends React.Component {
+
+const serverAddress = "127.0.0.1:5000";
+
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            id: null,
+            userName: null,
+            userType: null,
+        };
+    }
+
     render() {
+        if (this.state.id == null)
+            return this.renderLoginPage();
+        else {
+            switch (this.state.userType) {
+                case "Reader":
+                    return this.renderReaderPage();
+                case "Writer":
+                    return this.renderWriterPage();
+                case "Publisher":
+                    return this.renderPublisherPage();
+                default:
+                    alert("Unknown type!");
+            }
+        }
+    }
+
+    login() {
+        const formData = new FormData(document.getElementById("login-form"));
+        const username = formData.get("username");
+        if (username == '')
+            return;
+        else {
+            this.setState({
+                id: 1,
+                userName: username,
+                userType: "Reader",
+            });
+        }
+    }
+
+    renderLoginPage() {
         return (
             <div className="masthead d-flex">
                 <div className="container text-center my-auto">
-                    <h1 className="mb-1">Ligature</h1>
-                    <h3 className="mb-5">
+                    <h1>Ligature</h1>
+                    <h3 style={{ marginBottom: 3 + 'rem' }}>
                         <em>Publishing house & Book store</em>
                     </h3>
-                    <a className="btn btn-primary btn-xl js-scroll-trigger"
-                        data-toggle="modal" data-target="#LoginForm">Login</a>
+                    <form id="login-form" className="login-form">
+                        <input className="input" type="text" name="username" placeholder="Username"></input>
+                        <button className="login-form-btn" type="button" onClick={() => this.login()}>Login / Register</button>
+                        <div className="flex-col-c"></div>
+                    </form>
                 </div>
-                <div className="overlay"></div>
             </div>
-        )
+        );
+    }
+
+    renderReaderPage() {
+        return (
+            <div className="masthead d-flex">
+                <div className="container text-center my-auto">
+                    <form>
+                        <div className="row h-100">
+                            <div className="col-md-6 form-group text-center">
+                                <label htmlFor="Bought">Bought Books</label>
+                                <select size="5" className="form-control" id="Bought">
+                                    <option>First item</option>
+                                    <option>Second item</option>
+                                    <option>Third item</option>
+                                </select>
+                            </div>
+
+                            <div className="col-md-6 form-group text-center">
+                                <label htmlFor="Available">Available</label>
+                                <select size="5" className="form-control" id="Available">
+                                    <option>First item</option>
+                                    <option>Second item</option>
+                                    <option>Third item</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div className="container-login-form-btn">
+                            <button className="login-form-btn"
+                                style={{ width: "30%" }}
+                                type="button"
+                                onClick={() => this.buyBook()}>Buy book</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+
+    renderWriterPage() {
+        return (
+            <div className="masthead d-flex">
+                <div className="container text-center my-auto">
+                    <form>
+                        <div className="row h-100">
+                            <div className="col-md-4 form-group text-center">
+                                <label htmlFor="Published">Published</label>
+                                <select size="5" className="form-control" id="Published">
+                                    <option>First item</option>
+                                    <option>Second item</option>
+                                    <option>Third item</option>
+                                </select>
+                            </div>
+
+                            <div className="col-md-4 form-group text-center">
+                                <label htmlFor="Awaiting">Awaiting approval</label>
+                                <select size="5" className="form-control" id="Awaiting">
+                                    <option>First item</option>
+                                    <option>Second item</option>
+                                    <option>Third item</option>
+                                </select>
+                            </div>
+
+                            <div className="col-md-4 form-group text-center">
+                                <label htmlFor="Rejected">Rejected</label>
+                                <select size="5" className="form-control" id="Rejected">
+                                    <option>First item</option>
+                                    <option>Second item</option>
+                                    <option>Third item</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div className="container-login-form-btn">
+                            <button className="login-form-btn"
+                                style={{ width: "30%" }}
+                                type="button"
+                                onClick={() => this.publishBook()}>Publish book</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+
+    renderPublisherPage() {
+        return (
+            <div className="masthead d-flex">
+                <div className="container text-center my-auto">
+                    <form>
+                        <div className="row h-100">
+                            <div className="col-md-12 form-group text-center">
+                                <label htmlFor="Published">Published</label>
+                                <select size="5" className="form-control" id="Published">
+                                    <option>First item</option>
+                                    <option>Second item</option>
+                                    <option>Third item</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div className="row h-100">
+                            <div className="col-md-6 container-login-form-btn">
+                                <button className="login-form-btn"
+                                    style={{ width: "50%" }}
+                                    type="button"
+                                    onClick={() => this.publishBook()}>Publish book</button>
+                            </div>
+                            <div className="col-md-6 container-login-form-btn">
+                                <button className="login-form-btn"
+                                    style={{ width: "50%" }}
+                                    type="button"
+                                    onClick={() => this.publishBook()}>Publish book</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        );
     }
 }
 
+
 ReactDOM.render(
-    <MainScreen />,
+    <App />,
     document.getElementById('SPA')
 );
