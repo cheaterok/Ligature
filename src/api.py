68bd66d956bc4a1f5fec1a64ab7188a4c2a676ce
cheaterok <1cheaterok1@gmail.com>
2019-02-25 23:28:42 +0300

    Добавляет всё и даже больше

--- src/api.py
+++ src/api.py
@@ -12,33 +12,96 @@ def create_user(username):
     commit()  # Нужно закоммитить, чтобы получить id
     return {'id': new_user.id}
 
-@hug.get("/login")
-@db_session
-def login(username: hug.types.text):
-    user = select(user for user in User if user.name == username).first()
-    return {'id': user.id, 'name': user.name}
 
-@hug.local()
+@hug.get("/get_reader_data")
 @db_session
-def list_available_books(user_id):
+def get_reader_data(username: hug.types.text):
     "Перечисляет все книги, доступные для покупки пользователем."
-    current_user = User[user_id]
+    current_user = select(user for user in User if user.name == username).first()
+
+    if not current_user:
+        current_user = User(name=username)
+        commit()
+    
+    owned_books = select(book for book in Publication if book in current_user.owned_books or
+                                                      book in current_user.publications)
     available_books = select(book for book in Publication
                                 if book not in current_user.owned_books and 
-                                book not in current_user.publications)
-    return {'books': {'title': book.title for book in available_books}}
+                                book not in current_user.publications and
+                                book.status == 1)
+    return {
+        'id': current_user.id,
+        'owned_books': [book.title for book in owned_books],
+        'available_books': [book.title for book in available_books]
+        }
 
-@hug.local()
-def buy_book(user_id, book_id):
-    "Добавляет книгу в список купленных пользователем."
-
-@hug.local()
+@hug.get("/get_writer_data")
 @db_session
-def upload_book(user_id, title, content):
-    "Загружает книгу от имени пользователя."
-    book = Publication(title=title, author=User[user_id], content=content, status=1)
-    return book.__dict__
+def get_writer_data(username: hug.types.text):
+    "Перечисляет все книги, доступные для покупки пользователем."
+    current_user = select(user for user in User if user.name == username).first()
+    
+    all_books = select(book for book in Publication if book in current_user.publications)
 
-@hug.local()
-def review_book(user_id, book_id, answer):
-    "Оставляет вердикт по книге от имени издателя."
+    published_books = [book.title for book in all_books if book.status == 1]
+    awaiting_books = [book.title for book in all_books if book.status == 2]
+    rejected_books = [book.title for book in all_books if book.status == 3]
+
+    return {
+        'id': current_user.id,
+        'published_books': published_books,
+        'awaiting_books': awaiting_books,
+        'rejected_books': rejected_books
+        }
+
+@hug.get("/get_publisher_data")
+@db_session
+def get_publisher_data(username: hug.types.text):
+    "Перечисляет все книги, доступные для покупки пользователем."
+    current_user = select(user for user in User if user.name == username).first()
+    
+    awaiting_books = select(book for book in Publication if book.status == 2)
+
+
+    return {
+        'id': current_user.id,
+        'awaiting_books': [book.title for book in awaiting_books]
+        }
+
+@hug.get("/buy_book")
+@db_session
+def buy_book(username: hug.types.text, title: hug.types.text):
+        user = select(user for user in User if user.name == username).first()
+        book = select(book for book in Publication if book.title == title).first()
+
+        user.owned_books.add(book)
+
+        return {
+                "owned_books": user.owned_books
+        }
+
+@hug.post("/publish_book")
+@db_session
+def publish_book(body):
+        user = select(user for user in User if user.name == body['username']).first()
+
+        new_book = Publication(
+                title=body['title'],
+                author=user,
+                content=body['content'],
+                status=2
+        )
+
+        print(new_book.title, new_book.content)
+
+@hug.get("/accept_book")
+@db_session
+def accept_book(title: hug.types.text):
+        book = select(book for book in Publication if book.title == title).first()
+        book.status = 1
+
+@hug.get("/reject_book")
+@db_session
+def reject_book(title: hug.types.text):
+        book = select(book for book in Publication if book.title == title).first()
+        book.status = 3
--- web/js/react_components.js
+++ web/js/react_components.js
@@ -18,6 +18,8 @@ class App extends React.Component {
     render() {
         if (this.state.id == null)
             return this.renderLoginPage();
+        else if (this.state.special == "PublishBook")
+            return this.renderPublishPage();
         else {
             switch (this.state.userType) {
                 case "Reader":
@@ -36,19 +38,114 @@ class App extends React.Component {
         const formData = new FormData(document.getElementById("login-form"));
         const userName = formData.get("username");
         const userType = document.getElementById("select-type").value;
+
         if (userName == '')
             return;
         else {
-            fetch(serverAddress + "/login?username=" + userName)
-                .then((response) => response.json())
-                .then((json) =>
-                    this.setState({
-                        id: json.id,
-                        userName: json.name,
-                        userType: userType
-                    })
-                );
+            switch (userType) {
+                case "Reader":
+                    fetch(serverAddress + "/get_reader_data?username=" + userName)
+                        .then((response) => response.json())
+                        .then((json) => 
+                            this.setState({
+                                id: json.id,
+                                userName: userName,
+                                userType: userType,
+
+                                ownedBooks: json.owned_books,
+                                availableBooks: json.available_books
+                            })
+                        );
+                    break;
+                case "Writer":
+                    fetch(serverAddress + "/get_writer_data?username=" + userName)
+                        .then((response) => response.json())
+                        .then((json) => 
+                            this.setState({
+                                id: json.id,
+                                userName: userName,
+                                userType: userType,
+
+                                publishedBooks: json.published_books,
+                                awaitingBooks: json.awaiting_books,
+                                rejectedBooks: json.rejected_books
+                            })
+                        );
+                    break;
+                case "Publisher":
+                    fetch(serverAddress + "/get_publisher_data?username=" + userName)
+                        .then((response) => response.json())
+                        .then((json) => 
+                            this.setState({
+                                id: json.id,
+                                userName: userName,
+                                userType: userType,
+
+                                awaitingBooks: json.awaiting_books,
+                            })
+                        );
+                    break;
+                default:
+                    alert("Unknown type!");
+            }
         }
+    }
+
+    buyBook() {
+        const title = document.getElementById("Available").value;
+
+        fetch(serverAddress + "/buy_book?username=" + this.state.userName + "&title=" + title)
+            .then((response) => response.json())
+            .then((json) => 
+                this.setState({
+                    ownedBooks: json.owned_books
+                })
+            );
+    }
+
+    publishBook() {
+        const title = document.getElementById("title").value;
+        const content = document.getElementById("content").value;
+        console.log(title + "  " + content);
+
+        fetch(serverAddress + "/publish_book", {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                title: title,
+                content: content,
+                username: this.state.userName
+            })
+        });
+
+        var awaitingBooks = this.state.awaitingBooks;
+        awaitingBooks.push(title);
+
+        this.setState({
+            special: undefined, 
+            awaitingBooks: awaitingBooks
+        });
+    }
+
+    reviewBook(status) {
+        const title = document.getElementById("Awaiting").value;
+        console.log(title)
+
+        if (status == "Approve")
+            fetch(serverAddress + "/accept_book?title=" + title);
+        else if (status == "Reject")
+            fetch(serverAddress + "/reject_book?title=" + title);
+
+        fetch(serverAddress + "/get_publisher_data?username=" + this.state.userName)
+            .then((response) => response.json())
+            .then((json) => 
+                this.setState({
+                    awaitingBooks: json.awaiting_books,
+                })
+            );
     }
 
     renderLoginPage() {
@@ -82,19 +179,23 @@ class App extends React.Component {
                         <div className="row h-100">
                             <div className="col-md-6 form-group text-center">
                                 <label htmlFor="Bought">Bought Books</label>
-                                <select size="5" className="form-control" id="Bought">
-                                    <option>First item</option>
-                                    <option>Second item</option>
-                                    <option>Third item</option>
+                                <select size="5" className="form-control" id="Bought" disabled>
+                                if (this.state.ownedBooks.length != 0) {
+                                    this.state.ownedBooks.map((book) =>
+                                        <option>{book}</option>
+                                    )
+                                }
                                 </select>
                             </div>
 
                             <div className="col-md-6 form-group text-center">
                                 <label htmlFor="Available">Available</label>
                                 <select size="5" className="form-control" id="Available">
-                                    <option>First item</option>
-                                    <option>Second item</option>
-                                    <option>Third item</option>
+                                if (this.state.availableBooks.length != 0) {
+                                    this.state.availableBooks.map((book) =>
+                                        <option>{book}</option>
+                                    )
+                                }
                                 </select>
                             </div>
                         </div>
@@ -119,30 +220,55 @@ class App extends React.Component {
                             <div className="col-md-4 form-group text-center">
                                 <label htmlFor="Published">Published</label>
                                 <select size="5" className="form-control" id="Published">
-                                    <option>First item</option>
-                                    <option>Second item</option>
-                                    <option>Third item</option>
+                                if (this.state.publishedBooks.length != 0) {
+                                    this.state.publishedBooks.map((book) =>
+                                        <option>{book}</option>
+                                    )
+                                }
                                 </select>
                             </div>
 
                             <div className="col-md-4 form-group text-center">
                                 <label htmlFor="Awaiting">Awaiting approval</label>
                                 <select size="5" className="form-control" id="Awaiting">
-                                    <option>First item</option>
-                                    <option>Second item</option>
-                                    <option>Third item</option>
+                                if (this.state.awaitingBooks.length != 0) {
+                                    this.state.awaitingBooks.map((book) =>
+                                        <option>{book}</option>
+                                    )
+                                }
                                 </select>
                             </div>
 
                             <div className="col-md-4 form-group text-center">
                                 <label htmlFor="Rejected">Rejected</label>
                                 <select size="5" className="form-control" id="Rejected">
-                                    <option>First item</option>
-                                    <option>Second item</option>
-                                    <option>Third item</option>
+                                if (this.state.rejectedBooks.length != 0) {
+                                    this.state.rejectedBooks.map((book) =>
+                                        <option>{book}</option>
+                                    )
+                                }
                                 </select>
                             </div>
                         </div>
+                        <div className="container-login-form-btn">
+                            <button className="login-form-btn"
+                                style={{ width: "30%" }}
+                                type="button"
+                                onClick={() => this.setState({special: "PublishBook"})}>Publish book</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+
+    renderPublishPage() {
+        return (
+            <div className="masthead d-flex">
+                <div className="container text-center my-auto">
+                    <form className="container-login-from-btn">
+                        <input className="input" type="text" id="title" placeholder="Title"></input>
+                        <textarea cols="40" rows="3" id="content" placeholder="Content"></textarea>
                         <div className="container-login-form-btn">
                             <button className="login-form-btn"
                                 style={{ width: "30%" }}
@@ -152,7 +278,7 @@ class App extends React.Component {
                     </form>
                 </div>
             </div>
-        );
+        )
     }
 
     renderPublisherPage() {
@@ -162,26 +288,30 @@ class App extends React.Component {
                     <form>
                         <div className="row h-100">
                             <div className="col-md-12 form-group text-center">
-                                <label htmlFor="Published">Published</label>
-                                <select size="5" className="form-control" id="Published">
-                                    <option>First item</option>
-                                    <option>Second item</option>
-                                    <option>Third item</option>
+                                <label htmlFor="Awaiting">Publish requests</label>
+                                <select size="5" className="form-control" id="Awaiting">
+                                if (this.state.awaitingBooks.length != 0) {
+                                    this.state.awaitingBooks.map((book) =>
+                                        <option>{book}</option>
+                                    )
+                                }
                                 </select>
                             </div>
                         </div>
                         <div className="row h-100">
                             <div className="col-md-6 container-login-form-btn">
                                 <button className="login-form-btn"
+                                    id="Approve"
                                     style={{ width: "50%" }}
                                     type="button"
-                                    onClick={() => this.publishBook()}>Publish book</button>
+                                    onClick={() => this.reviewBook("Approve")}>Approve</button>
                             </div>
                             <div className="col-md-6 container-login-form-btn">
                                 <button className="login-form-btn"
+                                    id="Reject"
                                     style={{ width: "50%" }}
                                     type="button"
-                                    onClick={() => this.publishBook()}>Publish book</button>
+                                    onClick={() => this.reviewBook("Reject")}>Reject</button>
                             </div>
                         </div>
                     </form>
